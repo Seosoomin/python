d19b927a038199b9a921cb1649ac68b984c499eb
Seosoomin <tjtnals22@gmail.com>
2016-10-15 15:23:34 +0900

    Update turtel6.py

--- day1/turtel6.py
+++ day1/turtel6.py
@@ -1,48 +1,80 @@
 import turtle
 
 turtle.shape("turtle")
+turtle.speed(3)
+
+turtle.circle(100)
+
+turtle.right(90)
+turtle.penup()
+turtle.forward(50)
+turtle.pendown()
+turtle.left(90)
 
 turtle.circle(150)
 
 turtle.left(90)
 turtle.penup()
-turtle.forward(50)
+turtle.forward(150)
 turtle.pendown()
 
-turtle.right(90)
-turtle.circle(100)
+# 삼각형 1번째 그리기
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
 
+turtle.right(60)
 
-#1
-turtle.left(90)
+# 삼각형 2번째 그리기
 turtle.forward(100)
-turtle.left(120)
+turtle.right(120)
 turtle.forward(100)
-turtle.left(120)
+turtle.right(120)
 turtle.forward(100)
-turtle.left(60)
+turtle.right(120)
 
-turtle.forward(100)
-turtle.left(120)
-turtle.forward(200)
-turtle.left(120)
-turtle.forward(100)
-turtle.left(60)
+turtle.right(60)
 
+# 삼각형 3번째 그리기
 turtle.forward(100)
-turtle.left(120)
-turtle.forward(200)
-turtle.left(120)
+turtle.right(120)
 turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
 
-turtle.left(60)
+turtle.right(60)
 
+# 삼각형 4번째 그리기
 turtle.forward(100)
-turtle.left(120)
-turtle.forward(200)
-turtle.left(120)
+turtle.right(120)
 turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
 
+turtle.right(60)
+
+# 삼각형 5번째 그리기
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+
+turtle.right(60)
+
+# 삼각형 6번째 그리기
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
 
 
 
